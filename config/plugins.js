26893f7e7d82d495d966cf1060323c186c277a26
Jenny Leidig <jennymaeleidig@gmail.com>
2023-06-27 15:24:45 -0400

    adjusted git ignore and documentation

--- config/plugins.js
+++ config/plugins.js
@@ -2,6 +2,22 @@ module.exports = {
     documentation: {
         enabled: true,
         config: {
+            openapi: '3.0.0',
+            info: {
+                version: '1.0.0',
+                title: 'DOCUMENTATION',
+                description: '',
+                termsOfService: 'YOUR_TERMS_OF_SERVICE_URL',
+                contact: {
+                name: 'TEAM',
+                email: 'contact-email@something.io',
+                url: 'mywebsite.io'
+                },
+                license: {
+                name: 'Apache 2.0',
+                url: 'https://www.apache.org/licenses/LICENSE-2.0.html'
+                },
+            },
             "x-strapi-config": {
                 // Do not generate for plugins
                 plugins: [],
@@ -44,6 +60,12 @@ module.exports = {
                     };
                         
                 },
+            },
+            security: [ { bearerAuth: [] } ],
+            servers: [{ url: 'http://localhost:1337/api', description: 'Development server' }],
+            externalDocs: {
+                description: 'Find out more',
+                url: 'https://docs.strapi.io/developer-docs/latest/getting-started/introduction.html'
             },
         },
     },
