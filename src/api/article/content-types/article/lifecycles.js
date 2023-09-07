f54bf086c9ab0cb2b79fd2bc629eb37834dbe3f2
Jenny Leidig <jennymaeleidig@gmail.com>
2023-09-06 23:12:07 -0400

    HOTFIX: fixed infiite lifecycle update loop when setting readtime

--- src/api/article/content-types/article/lifecycles.js
+++ src/api/article/content-types/article/lifecycles.js
@@ -5,11 +5,16 @@ const { default: readTimeEstimate } = require('read-time-estimate');
 async function updateReadTime(event){
   const { result, params } = event; 
   
-    const entry = await strapi.entityService.update('api::article.article', result.id, {
-      data: {
-        readTime: readTimeEstimate(result.body, 190, 12, 500, ['img', 'Image']).duration.toFixed(2)
-      },
-    });
+    const uid = "api::article.article";
+    const readTime = readTimeEstimate(result.body, 190, 12, 500, ['img', 'Image']).duration.toFixed(2)
+
+    //update via knex so that the update lifecycle function isnt triggered repeatedly (forever)
+    const rand = await strapi.db.connection
+        .table(strapi.getModel(uid).collectionName)
+        .update({
+          read_time: readTime
+        })
+        .where('id', result.id)
 }
 
 module.exports = {
